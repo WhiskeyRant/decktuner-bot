624f49e7a064e4f034e066c2ec07e6fab466a1d2
WhiskeyRant <utturkey@yahoo.com>
2021-06-09 14:55:47 -0700

    added production configurations and other minor changes

--- src/data/settings.js
+++ src/data/settings.js
@@ -2,20 +2,25 @@
 const settings = {
     channels: {
         get_help: {
-            development: "846013084896067614"
+            development: "846013084896067614",
+            production: "847356745986867240"
         },
         bounty_board: {
-            development: "846020389062443028"
+            development: "846020389062443028",
+            production: "851479909532434453"
         },
         workshop_category: {
-            development: "847460751940780072"
+            development: "847460751940780072",
+            production: "852304713478176779"
         },
         history: {
-            development: "850215136962936902"
+            development: "850215136962936902",
+            production: "852304818553749505"
         }
     },
     servers: {
-        development: "845671055384772698"
+        development: "845671055384772698",
+        production: "845023235422421056"
     },
     images: {
         bot_logo: "https://i.imgur.com/ZgRIZKY.png"
