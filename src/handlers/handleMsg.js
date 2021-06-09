1f56a70e936e9c7ef22b8eb1e467afb4d71c8175
WhiskeyRant <utturkey@yahoo.com>
2021-06-09 15:00:38 -0700

    fixed missing dependency import

--- src/handlers/handleMsg.js
+++ src/handlers/handleMsg.js
@@ -4,74 +4,11 @@ import settings from '../../src/data/settings';
 import bountyListing from '../embeds/bountyListing';
 import createRoom from '../utils/createRoom';
 import closeWorkshop from '../utils/closeWorkshop';
-import initDB from '../db/init';
 import tunerParticipation from '../utils/tunerParticipation';
 import ActiveInterviews from '../utils/ActiveInterviews';
-const nodeHtmlToImage = require('node-html-to-image');
-const { MessageAttachment } = require('discord.js')
 
 const handleMsg = async (msg) => {
     try {
-        if (msg.content.startsWith('!test')) {
-            const _htmlTemplate = `<!DOCTYPE html>
-            <html lang="en">
-              <head>
-                <meta charset="UTF-8" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-                <style>
-                  body {
-                    font-family: "Poppins", Arial, Helvetica, sans-serif;
-                    background: rgb(22, 22, 22);
-                    color: #fff;
-                    max-width: 300px;
-                  }
-            
-                  .app {
-                    max-width: 300px;
-                    padding: 20px;
-                    display: flex;
-                    flex-direction: row;
-                    border-top: 3px solid rgb(16, 180, 209);
-                    background: rgb(31, 31, 31);
-                    align-items: center;
-                  }
-            
-                  img {
-                    width: 50px;
-                    height: 50px;
-                    margin-right: 20px;
-                    border-radius: 50%;
-                    border: 1px solid #fff;
-                    padding: 5px;
-                  }
-                </style>
-              </head>
-              <body>
-                <div class="app">
-                  <img src="https://i.imgur.com/4zSAWJ5.jpg" />
-            
-                  <h4>Welcome dog</h4>
-                </div>
-              </body>
-            </html>
-            `;
-
-            const images = await nodeHtmlToImage({
-                html: _htmlTemplate,
-                quality: 100,
-                type: 'jpeg',
-                puppeteerArgs: {
-                    args: ['--no-sandbox'],
-                },
-                encoding: 'buffer',
-            });
-            // for more configuration options refer to the library
-
-            return msg.channel.send(
-                new MessageAttachment(images, `dog.jpeg`)
-            );
-        }
         if (
             msg.content.startsWith('!tune') &&
             settings.channel('get_help') === msg.channel.id
